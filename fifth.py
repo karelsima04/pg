2e459dd7aa39dd5f2171db76e8f901212bfd31c5
karelsima04 <simaka03@jcu.cz>
2025-11-14 00:53:03 +0000

    ukol5

---
+++ fifth.py
@@ -0,0 +1,51 @@
+import sys
+
+jpeg_header = b'\xff\xd8'
+gif_header1 = b'GIF87a'
+gif_header2 = b'GIF89a'
+png_header = b'\x89PNG\r\n\x1a\n'
+
+
+def read_header(file_name, header_length):
+    with open(file_name, 'rb') as f:
+        header = f.read(header_length)
+    return header
+
+
+def is_jpeg(file_name):
+    header = read_header(file_name, len(jpeg_header))
+    return header == jpeg_header
+
+
+def is_gif(file_name):
+    header = read_header(file_name, len(gif_header1))
+    return header == gif_header1 or header == gif_header2
+
+
+def is_png(file_name):
+    header = read_header(file_name, len(png_header))
+    return header == png_header
+
+
+def print_file_type(file_name):
+    if is_jpeg(file_name):
+        print(f'Soubor {file_name} je jpeg')
+    elif is_gif(file_name):
+        print(f'Soubor {file_name} je gif')
+    elif is_png(file_name):
+        print(f'Soubor {file_name} je png')
+    else:
+        print(f'Soubor {file_name} je neplatnej')
+
+
+if __name__ == "__main__":
+    try:
+        file_name = sys.argv[1]
+        print_file_type(file_name)
+    except IndexError:
+        print("Chyba Nebyl zadán název souboru.")
+        print("Použití python fifth.py <cesta_k_souboru>")
+    except FileNotFoundError:
+        print(f"Chyba: Soubor '{sys.argv[1]}' nebyl nalezen.")
+    except Exception as e:
+        print(f"Došlo k chybeˇ: {e}")
