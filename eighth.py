eb15bb4dfa9cf59141652785bee5ec6ecc586c3f
karelsima04 <simaka03@jcu.cz>
2025-12-13 21:58:18 +0000

    hotovson

---
+++ eighth.py
@@ -0,0 +1,17 @@
+def bin_to_dec(binarni_cislo):
+    binarni_cislo = str(binarni_cislo)
+    vysledek = 0
+
+    for cifra in binarni_cislo:
+        vysledek = vysledek * 2 + int(cifra)
+
+    return vysledek
+
+
+def test_bin_to_dec():
+    assert bin_to_dec("0") == 0
+    assert bin_to_dec(1) == 1
+    assert bin_to_dec("100") == 4
+    assert bin_to_dec(101) == 5
+    assert bin_to_dec("010101") == 21
+    assert bin_to_dec(10000000) == 128
